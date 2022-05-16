18829da2cc1580d92fca9942906b3325d4f04739
cory-tidwell <101146141+cory-tidwell@users.noreply.github.com>
2022-05-16 18:01:04 -0400

    java-intro-exercises

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,44 @@
+import java.awt.desktop.SystemSleepEvent;
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %s", pi);
+//        System.out.format("The value of pi is approximately %.2f%n", pi);
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter an number: ");
+
+
+        String input1;
+        String input2;
+        String input3;
+//
+        Scanner scanner = new Scanner(System.in);
+//        System.out.printf("Enter three words: ");
+//
+//        System.out.print("Enter your name: ");
+//        input1 = scanner.next();
+//        input2 = scanner.next();
+//        input3 = scanner.next();
+////        int userInput = scanner.nextInt();
+//        System.out.printf("Your three words are: \n%s \n%s\n%s", input1, input2, input3);
+//        System.out.println("Your number is: " + userInput + "!");
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Please enter a sentence: ");
+//        String userString = scanner.nextLine();
+//        System.out.printf("Your sentence: " + userString);
+
+        System.out.println("Enter the width of your room: ");
+        String widthInput = scanner.nextLine();
+        System.out.println("Enter the length of your room: ");
+        String lengthInput = scanner.nextLine();
+        int area = Integer.valueOf(widthInput) * (Integer.valueOf(lengthInput));
+        int perimeter =  (Integer.valueOf(widthInput) * 2) + (Integer.valueOf(lengthInput) * 2);
+        System.out.printf("The width entered was %s, the length entered was %s.", widthInput, lengthInput);
+        System.out.printf("\nThe area of the room is %s, the perimeter is %s.", area, perimeter);
+
+    }
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,49 @@
 public class HelloWorld {
         public static void main(String[] args) {
+            String name = "Cory";
+
+            int[] numbers = {10, 20, 30};
             System.out.println("Hello, World!");
-            System.out.print("Hello");
+            System.out.println(name);
+//            System.out.print(numbers[0]);
+//
+//            int myFavoriteNumber = 3;
+//            System.out.println(myFavoriteNumber);
+//
+//            String myString = "This is my string!"; //cannot assign as char or a number
+//            System.out.println(myString);
+//
+//            float myNumber = 3.14F; //cannot assign 3.14 to a long
+//            System.out.println(myNumber); //needs a value
+//
+////            int x = 5;
+////            System.out.println(x++);
+////            System.out.println(x);
+//
+//            int x = 5;
+//            System.out.println(++x);
+//            System.out.println(x);
+
+//            String theNumberThree = "three";
+//            Object o = theNumberThree;
+//            int three = (int) o;
+//
+//            System.out.println(o);
+//
+//            int x = 4+5;
+//            System.out.println(x);
+//
+//            int y = 3*4;
+//            System.out.println(y);
+
+//            int x = 10/2;
+//            int y = 2-10;
+//            System.out.println(x);
+//            System.out.println(y);
+
+            int x = 2147483647;
+            System.out.println(++x); //prints negative -2147483648
+
         }
     }
 
