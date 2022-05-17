4bf55f61a4b5af162b7599545fd2d48b7830c2b5
cory-tidwell <101146141+cory-tidwell@users.noreply.github.com>
2022-05-17 15:21:03 -0400

    java-conditions

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
         //*** While Loop ***
@@ -35,16 +37,52 @@ public class ControlFlowExercises {
 //            System.out.println(c);
 //        }
           //*** FizzBuzz ***
-        for(int i = 0; i <= 100; i++) {
-            if ((i % 5 == 0) && (i % 3 == 0)) {
-                System.out.println("FizzBuzz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else {
-                System.out.println(i);
+//        for(int i = 0; i <= 100; i++) {
+//            if ((i % 5 == 0) && (i % 3 == 0)) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+            //*** Table of Powers ***
+        Scanner scanner = new Scanner(System.in);
+//        boolean continuePlaying;
+//        do{
+//            System.out.print("What number would you like to go up to? ");
+//            int userInput = scanner.nextInt();
+//            System.out.println("number \t | squared \t | cubed \n ----- \t | -----\t | ----- ");
+//            for(int i = 1; i <= userInput; i++){
+//                System.out.printf("%s \t \t | %s \t \t | %s \n", i, ((int )Math.pow(i, 2)), ((int)Math.pow(i, 3)));
+//            }
+//            System.out.println("Press y to continue");
+//            String userContinue = scanner.next();
+//            continuePlaying = userContinue.equalsIgnoreCase("y");
+//        }while(continuePlaying);
+        //*** Grades ***
+        boolean studentContinue;
+        do{
+            System.out.print("Enter your grade (from 0 to 100): ");
+            int studentGrade = scanner.nextInt();
+            if(studentGrade >= 88 && studentGrade <= 100){
+                System.out.println("You got an A!");
+            }else if (studentGrade < 88 && studentGrade >= 80){
+                System.out.println("You got a B.");
+            }else if (studentGrade < 80 && studentGrade >= 67){
+                System.out.println("You got a C.");
+            }else if(studentGrade < 67 && studentGrade >= 60){
+                System.out.println("You got a D.");
+            }else if(studentGrade < 60){
+                System.out.println("You got an F.");
+            }else {
+                System.out.println("Something isn't right");
             }
-        }
+            System.out.println("Would you like to continue? enter y");
+            String continueGrades = scanner.next();
+            studentContinue = continueGrades.equalsIgnoreCase("y");
+        }while(studentContinue);
     }
 }
