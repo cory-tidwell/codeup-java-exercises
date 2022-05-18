bc74e7f4d1acdff82b15f6ea6e568742eea5b182
cory-tidwell <101146141+cory-tidwell@users.noreply.github.com>
2022-05-18 18:26:14 -0400

    java-exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -8,6 +8,37 @@ import java.io.IOException;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        //*** JAVA ASCII ***
+        int width = 100;
+        int height = 30;
+
+        //BufferedImage image = ImageIO.read(new File("/Users/mkyong/Desktop/logo.jpg"));
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics g = image.getGraphics();
+        g.setFont(new Font("SansSerif", Font.BOLD, 24));
+
+        Graphics2D graphics = (Graphics2D) g;
+        graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        graphics.drawString("CORY", 10, 20);
+
+        //save this image
+        //ImageIO.write(image, "png", new File("/users/mkyong/ascii-art.png"));
+
+        for (int y = 0; y < height; y++) {
+            StringBuilder sb = new StringBuilder();
+            for (int x = 0; x < width; x++) {
+
+                sb.append(image.getRGB(x, y) == -16777216 ? " " : "$");
+
+            }
+
+            if (sb.toString().trim().isEmpty()) {
+                continue;
+            }
+
+            System.out.println(sb);
+        }
         //*** While Loop ***
 //        int i = 5;
 //        while (i <= 15) {
@@ -56,70 +87,40 @@ public class ControlFlowExercises {
 //        }
             //*** Table of Powers ***
         Scanner scanner = new Scanner(System.in);
-//        boolean continuePlaying;
-//        do{
-//            System.out.print("What number would you like to go up to? ");
-//            int userInput = scanner.nextInt();
-//            System.out.println("number \t | squared \t | cubed \n ----- \t | -----\t | ----- ");
-//            for(int i = 1; i <= userInput; i++){
-//                System.out.printf("%s \t \t | %s \t \t | %s \n", i, ((int )Math.pow(i, 2)), ((int)Math.pow(i, 3)));
-//            }
-//            System.out.println("Press y to continue");
-//            String userContinue = scanner.next();
-//            continuePlaying = userContinue.equalsIgnoreCase("y");
-//        }while(continuePlaying);
-        //*** JAVA ASCII ***
-        int width = 100;
-        int height = 30;
-
-        //BufferedImage image = ImageIO.read(new File("/Users/mkyong/Desktop/logo.jpg"));
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        Graphics g = image.getGraphics();
-        g.setFont(new Font("SansSerif", Font.BOLD, 24));
-
-        Graphics2D graphics = (Graphics2D) g;
-        graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        graphics.drawString("CORY", 10, 20);
-
-        //save this image
-        //ImageIO.write(image, "png", new File("/users/mkyong/ascii-art.png"));
-
-        for (int y = 0; y < height; y++) {
-            StringBuilder sb = new StringBuilder();
-            for (int x = 0; x < width; x++) {
-
-                sb.append(image.getRGB(x, y) == -16777216 ? " " : "$");
-
-            }
-
-            if (sb.toString().trim().isEmpty()) {
-                continue;
-            }
-
-            System.out.println(sb);
-        }
-        //*** Grades ***
-        boolean studentContinue;
+        boolean continuePlaying;
         do{
-            System.out.print("\nEnter your grade (from 0 to 100): ");
-            int studentGrade = scanner.nextInt();
-            if(studentGrade >= 88 && studentGrade <= 100){
-                System.out.println("You got an A!");
-            }else if (studentGrade < 88 && studentGrade >= 80){
-                System.out.println("You got a B.");
-            }else if (studentGrade < 80 && studentGrade >= 67){
-                System.out.println("You got a C.");
-            }else if(studentGrade < 67 && studentGrade >= 60){
-                System.out.println("You got a D.");
-            }else if(studentGrade < 60){
-                System.out.println("You got an F.");
-            }else {
-                System.out.println("Good Bye!");
+            System.out.print("What number would you like to go up to? ");
+            int userInput = scanner.nextInt();
+            System.out.println("number \t | squared \t | cubed \n ----- \t | -----\t | ----- ");
+            for(int i = 1; i <= userInput; i++){
+                System.out.printf("%s \t \t | %s \t \t | %s \n", i, ((int )Math.pow(i, 2)), ((int)Math.pow(i, 3)));
             }
-            System.out.println("Would you like to continue? enter y");
-            String continueGrades = scanner.next();
-            studentContinue = continueGrades.equalsIgnoreCase("y");
-        }while(studentContinue);
+            System.out.println("Press y to continue");
+            String userContinue = scanner.next();
+            continuePlaying = userContinue.equalsIgnoreCase("y");
+        }while(continuePlaying);
+
+        //*** Grades ***
+//        boolean studentContinue;
+//        do{
+//            System.out.print("\nEnter your grade (from 0 to 100): ");
+//            int studentGrade = scanner.nextInt();
+//            if(studentGrade >= 88 && studentGrade <= 100){
+//                System.out.println("You got an A!");
+//            }else if (studentGrade < 88 && studentGrade >= 80){
+//                System.out.println("You got a B.");
+//            }else if (studentGrade < 80 && studentGrade >= 67){
+//                System.out.println("You got a C.");
+//            }else if(studentGrade < 67 && studentGrade >= 60){
+//                System.out.println("You got a D.");
+//            }else if(studentGrade < 60){
+//                System.out.println("You got an F.");
+//            }else {
+//                System.out.println("Good Bye!");
+//            }
+//            System.out.println("Would you like to continue? enter y");
+//            String continueGrades = scanner.next();
+//            studentContinue = continueGrades.equalsIgnoreCase("y");
+//        }while(studentContinue);
     }
 }
