2f90f3527c3d8e11ec9ea1f841e85e097bdcf45f
cory-tidwell <101146141+cory-tidwell@users.noreply.github.com>
2022-05-27 19:02:19 -0400

    self-stufy

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,16 @@
 import java.lang.StringBuilder;
 
 class Solution {
-    static
+    static String toCamelCase(String s){
+
+        // Variables
+        String camelCaseString;
+        StringBuilder sb = new StringBuilder(s);
+
+        // Algorithm start here
+
+        camelCaseString = sb.toString().replaceAll("[-_]", "");
+
+        return camelCaseString;
+    }
 }
