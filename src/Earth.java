3adb490481ecb02b0041dae2def1dd954f7aebdc
cory-tidwell <101146141+cory-tidwell@users.noreply.github.com>
2022-06-28 11:44:40 -0400

    java-practice

---
+++ src/Earth.java
@@ -0,0 +1,16 @@
+public class Earth {
+    public static void main(String[] args) {
+
+       Human human1 = new Human("Cory", 39, 75, "Brown");
+       Human human2 = new Human("Tom", 25, 76, "Blue");
+       Human human3 = new Human("Joe", 28, 68, "Green");
+
+       human1.speak();
+        System.out.println();
+       human2.speak();
+        System.out.println();
+       human3.speak();
+
+
+    }
+}
